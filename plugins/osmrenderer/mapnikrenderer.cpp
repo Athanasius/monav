d67cf066fe20c43240950da0422a5bef2bf3b303
Athanasius <code@miggy.org>
2016-09-03 14:58:33 +0100

    osmrenderer/mapnikrenderer.cpp: Object not pointer

--- plugins/osmrenderer/mapnikrenderer.cpp
+++ plugins/osmrenderer/mapnikrenderer.cpp
@@ -147,7 +147,7 @@ bool MapnikRenderer::Preprocess( IImporter* importer, QString dir )
 
 		Timer time;
 
-		mapnik::datasource_cache::instance()->register_datasources( m_settings.plugins.toAscii().constData() );
+		mapnik::datasource_cache::instance().register_datasources( m_settings.plugins.toAscii().constData() );
 		QDir fonts( m_settings.fonts );
 		mapnik::projection projection;
 		projection = mapnik::projection( "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs +over" );
